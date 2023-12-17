74089118d50d10db05a0eda98877c304612dca4e
Nasina-Srilahari <nasinasrilahariias@gmail.com>
2023-12-17 17:53:17 +0530

    images update

--- script.js
+++ script.js
@@ -10,18 +10,43 @@ menu.onclick = () => {
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
-// Dark Mode / light mode
-let darkmode = document.querySelector("#darkmode");
-darkmode.onclick = () => {
-  if (darkmode.classList.contains("bx-moon")) {
-    darkmode.classList.replace("bx-moon", "bx-sun");
-    document.body.classList.add("active");
-  } else {
-    darkmode.classList.replace("bx-sun", "bx-moon");
-    document.body.classList.remove("active");
-  }
-};                            
+// // Dark Mode / light mode
+// let darkmode = document.querySelector("#darkmode");
+// darkmode.onclick = () => {
+//   if (darkmode.classList.contains("bx-moon")) {
+//     darkmode.classList.replace("bx-moon", "bx-sun");
+//     document.body.classList.add("active");
+//   } else {
+//     darkmode.classList.replace("bx-sun", "bx-moon");
+//     document.body.classList.remove("active");
+//   }
+// };                            
  
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Dark Mode / light mode
+  let darkmode = document.querySelector("#darkmode");
+
+  // Function to toggle dark mode
+  function toggleDarkMode() {
+    if (darkmode.classList.contains("bx-moon")) {
+      darkmode.classList.replace("bx-moon", "bx-sun");
+      document.body.classList.add("active");
+    } else {
+      darkmode.classList.replace("bx-sun", "bx-moon");
+      document.body.classList.remove("active");
+    }
+  }
+
+  // Initially set the page to dark mode
+  toggleDarkMode();
+
+  // Event listener for dark mode toggle
+  darkmode.onclick = toggleDarkMode;
+
+  // Rest of your existing code...
+
+  // ...
+});
 
 
